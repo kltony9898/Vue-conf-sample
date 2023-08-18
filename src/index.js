c344029a957d10db8cba99dc6520273abfc7758d
kltony9898 <ashe@osensetech.com>
2023-08-18 15:51:00 +0800

    feat: tags-input

--- src/index.js
+++ src/index.js
@@ -2,3 +2,4 @@ import "virtual:uno.css";
 import "@unocss/reset/eric-meyer.css";
 export { default as MEmailInput } from "./components/MEmailInput.vue";
 export { default as MPureTable } from "./components/MPureTable.vue";
+export { default as FormTagsInput } from "./components/FormTagsInput.vue";
